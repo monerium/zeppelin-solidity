cc6755b77360628e8948e55beb6e3ca2b93311b2
Jorge Izquierdo <jorge@izqui.me>
2017-03-09 17:37:18 +0100

    Try to make totalSupply a function of the underlying token

--- contracts/token/WrappedToken.sol
+++ contracts/token/WrappedToken.sol
@@ -12,7 +12,7 @@ contract WrappedToken is StandardToken {
     uint256 wrappingAmount = token.allowance(msg.sender, this);
     if (wrappingAmount < 1) throw;
     if (!token.transferFrom(msg.sender, address(this), wrappingAmount)) throw;
-    totalSupply = safeAdd(totalSupply, wrappingAmount);
+    // totalSupply = safeAdd(totalSupply, wrappingAmount);
     balances[msg.sender] = safeAdd(balances[msg.sender], wrappingAmount);
   }
 
@@ -22,10 +22,18 @@ contract WrappedToken is StandardToken {
 
   function unwrapAndTransfer(address receiver, uint amount) {
     // if (amount > balances[msg.sender]) throw; // Implicitely checked by safeSub(balance[msg.sender], amount)
-    totalSupply = safeSub(totalSupply, amount);
+    // totalSupply = safeSub(totalSupply, amount);
     balances[msg.sender] = safeSub(balances[msg.sender], amount);
     if (!token.transfer(receiver, amount)) throw;
   }
+
+  /*
+  // Doesn't work :(
+  function totalSupply() constant returns (uint) {
+    return token.totalSupply();
+  }
+  */
+
 
   ERC20 token;
 }
