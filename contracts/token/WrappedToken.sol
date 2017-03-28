2b2b390c6ac0f547ae334f619808367b852cb092
Jorge Izquierdo <jorge@izqui.me>
2017-03-28 12:38:25 +0200

    Update

--- contracts/token/WrappedToken.sol
+++ contracts/token/WrappedToken.sol
@@ -2,18 +2,24 @@ pragma solidity ^0.4.8;
 
 import "./StandardToken.sol";
 
-contract WrappedToken is StandardToken {
+contract ERC20Wrap is ERC20 {
+  function parentToken() constant public returns (address);
+  function parentTotalSupply() constant public returns (uint256);
+}
+
+contract WrappedToken is ERC20Wrap, StandardToken {
   function WrappedToken(address _token) {
     token = ERC20(_token);
   }
 
   // previous to wrap msg.sender must create an allowance for wrapper
-  function wrap() {
-    uint256 wrappingAmount = token.allowance(msg.sender, this);
-    if (wrappingAmount < 1) throw;
+  function wrap(uint256 wrappingAmount) {
+    uint256 availableAllowance = token.allowance(msg.sender, this);
+    if (wrappingAmount > availableAllowance) throw;
     if (!token.transferFrom(msg.sender, address(this), wrappingAmount)) throw;
-    // totalSupply = safeAdd(totalSupply, wrappingAmount);
+    totalSupply = safeAdd(totalSupply, wrappingAmount);
     balances[msg.sender] = safeAdd(balances[msg.sender], wrappingAmount);
+    Transfer(0x0, msg.sender, wrappingAmount);
   }
 
   function unwrap(uint amount) {
@@ -22,14 +28,19 @@ contract WrappedToken is StandardToken {
 
   function unwrapAndTransfer(address receiver, uint amount) {
     // if (amount > balances[msg.sender]) throw; // Implicitely checked by safeSub(balance[msg.sender], amount)
-    // totalSupply = safeSub(totalSupply, amount);
+    totalSupply = safeSub(totalSupply, amount);
     balances[msg.sender] = safeSub(balances[msg.sender], amount);
     if (!token.transfer(receiver, amount)) throw;
+    Transfer(msg.sender, 0x0, amount);
   }
 
-  function totalSupply() constant returns (uint) {
+  function parentTotalSupply() constant public returns (uint256) {
     return token.totalSupply();
   }
 
-  ERC20 token;
+  function parentToken() constant public returns (address) {
+    return address(token);
+  }
+
+  ERC20 public token;
 }
