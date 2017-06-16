82931ef1e583372ed78a8de5348d7331f147e3b3
Gísli Kristjánsson <gislik@hamstur.is>
2017-06-16 16:02:31 +0000

    Adding a `_caller` argument to some of the functions

--- contracts/token/ERC20Lib.sol
+++ contracts/token/ERC20Lib.sol
@@ -11,35 +11,48 @@ library ERC20Lib {
         uint totalSupply;
     }
 
-    function init(TokenStorage storage self, uint _initial_supply) {
+    function init(TokenStorage storage self, address _caller, uint _initial_supply) {
         self.totalSupply = _initial_supply;
-        self.balances[msg.sender] = _initial_supply;
+        self.balances[_caller] = _initial_supply;
     }
 
-    function transfer(TokenStorage storage self, address _to, uint _value) returns (bool success) {
-        self.balances[msg.sender] = self.balances[msg.sender].minus(_value);
+    function transfer(TokenStorage storage self, address _caller, address _to, uint _value) 
+        returns (bool success) 
+    {
+        self.balances[_caller] = self.balances[_caller].minus(_value);
         self.balances[_to] = self.balances[_to].plus(_value);
-        Transfer(msg.sender, _to, _value);
+        Transfer(_caller, _to, _value);
         return true;
     }
 
-    function transferFrom(TokenStorage storage self, address _from, address _to, uint _value) returns (bool success) {
-        var _allowance = self.allowed[_from][msg.sender];
+    function transferFrom(
+        TokenStorage storage self, 
+        address _caller, 
+        address _from, 
+        address _to, 
+        uint _value
+    ) returns (bool success) {
+        var _allowance = self.allowed[_from][_caller];
 
         self.balances[_to] = self.balances[_to].plus(_value);
         self.balances[_from] = self.balances[_from].minus(_value);
-        self.allowed[_from][msg.sender] = _allowance.minus(_value);
+        self.allowed[_from][_caller] = _allowance.minus(_value);
         Transfer(_from, _to, _value);
         return true;
     }
 
-    function balanceOf(TokenStorage storage self, address _owner) constant returns (uint balance) {
+    function balanceOf(TokenStorage storage self, address _owner) 
+        constant 
+        returns (uint balance) 
+    {
         return self.balances[_owner];
     }
 
-    function approve(TokenStorage storage self, address _spender, uint _value) returns (bool success) {
-        self.allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function approve(TokenStorage storage self, address _caller, address _spender, uint _value) 
+        returns (bool success) 
+    {
+        self.allowed[_caller][_spender] = _value;
+        Approval(_caller, _spender, _value);
         return true;
     }
 
