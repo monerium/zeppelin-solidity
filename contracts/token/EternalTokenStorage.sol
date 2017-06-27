81752431ea96e6f542537bf288785a876e81bef1
Gísli Kristjánsson <gislik@hamstur.is>
2017-06-27 22:55:44 +0000

    Removing files which have been copied to a local contracts repository

--- contracts/token/EternalTokenStorage.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-contract EternalTokenStorage {
-    struct TokenStorage {
-        mapping (address => uint) balances;
-        mapping (address => mapping (address => uint)) allowed;
-        uint totalSupply;
-    }
-}
